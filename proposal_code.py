3ee69943699d666e961513a0c41982d296431100
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-12-01 12:16:30 +0200

    getting proposal code from SALT Proposal

--- proposal_code.py
+++ proposal_code.py
@@ -1,8 +1,17 @@
 from xml.etree import ElementTree
+from zipfile import ZipFile
 
 
 def get_proposal_code(proposal):
-    tree = ElementTree.parse(proposal)
+    archive = ZipFile(proposal, "r")
+    file = archive.open("Proposal.xml")
+    if not file:
+        raise ValueError("There is no XML file containing the proposal code")
+    tree = ElementTree.parse(file)
     root = tree.getroot()
     proposal_code = root.attrib['code']
+    if proposal_code[0] != "2" or proposal_code.startswith("Unsubmitted-"):
+        raise ValueError("Invalid proposal code")
+    elif "Unsubmitted-" in proposal_code:
+        return None
     return proposal_code
