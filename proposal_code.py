11215ec111f3df2f37aa02896b917d60859c93ed
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-12-02 16:14:27 +0200

    getting proposal code from SALT Proposal

--- proposal_code.py
+++ proposal_code.py
@@ -3,15 +3,22 @@ from zipfile import ZipFile
 
 
 def get_proposal_code(proposal):
-    archive = ZipFile(proposal, "r")
-    file = archive.open("Proposal.xml")
-    if not file:
-        raise ValueError("There is no XML file containing the proposal code")
+    try:
+        archive = ZipFile(proposal, "r")
+        file = archive.open("Proposal.xml")
+    except FileNotFoundError:
+        raise ValueError("The zipped folder contains no file"
+                         " called Proposal.xml") from None
+
     tree = ElementTree.parse(file)
     root = tree.getroot()
     proposal_code = root.attrib['code']
-    if proposal_code[0] != "2" or proposal_code.startswith("Unsubmitted-"):
-        raise ValueError("Invalid proposal code")
-    elif "Unsubmitted-" in proposal_code:
+    if "code" not in root.attrib:
+        raise ValueError("Proposal code is not supplied in the file Proposal.xml")
+    elif proposal_code.startswith("2"):
+        return proposal_code
+    elif proposal_code.startswith("Unsubmitted-") or proposal_code == "":
         return None
-    return proposal_code
+    else:
+        return ValueError("Invalid proposal code from the file"
+                          " in zipped folder (Proposal.xml) ")
