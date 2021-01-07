2c4770793c6624ae5bd1833a4110fff92a1d6ff9
Lonwabo Zaula <lonwabozaula@gmail.com>
2021-01-07 13:42:31 +0200

    proposal code script

--- saltapi/repository/proposal_repository.py
+++ saltapi/repository/proposal_repository.py
@@ -3,6 +3,7 @@
 from typing import BinaryIO, Optional, Union
 from xml.etree import ElementTree
 from zipfile import ZipFile
+import zipfile
 
 
 def get_proposal_code(user_proposal: Union[str, BinaryIO]) -> Optional[str]:
@@ -11,12 +12,19 @@ def get_proposal_code(user_proposal: Union[str, BinaryIO]) -> Optional[str]:
         archive = ZipFile(user_proposal, "r")
         file = archive.open("Proposal.xml")
     except FileNotFoundError:
-        raise ValueError("The zipfile contains no file Proposal.xml.") from None
+        raise KeyError("There is no item named 'Proposal.xml' in the archive") from None
 
     tree = ElementTree.parse(file)
     proposal = tree.getroot()
     if "code" not in proposal.attrib:
         raise ValueError("No proposal code supplied in the file Proposal.xml.")
+
+    is_file_zipped = zipfile.is_zipfile(user_proposal)
+    if not is_file_zipped:
+        raise ValueError("The file supplied is not a zip file")
+
+    if proposal.tag.split("}")[1] != "Proposal":
+        raise ValueError("The root element of your xml file is not called Proposal")
 
     proposal_code = proposal.attrib["code"]
 
