9a737dc8cfee89ca1a5136aab7d3789efb24c85b
Lonwabo Zaula <lonwabozaula@gmail.com>
2021-01-12 11:34:57 +0200

    proposal code script

--- saltapi/repository/proposal_repository.py
+++ saltapi/repository/proposal_repository.py
@@ -1,26 +1,26 @@
 """Access to proposal information."""
 
 from typing import BinaryIO, Optional, Union
-from xml.etree import ElementTree
+from defusedxml.ElementTree import parse
 from zipfile import ZipFile
 import zipfile
 
 
-def get_proposal_code(user_proposal: Union[str, BinaryIO]) -> Optional[str]:
+def get_proposal_code(proposal_zip: Union[str, BinaryIO]) -> Optional[str]:
     """Extract the proposal code from a proposal file."""
+    if not zipfile.is_zipfile(proposal_zip):
+        raise ValueError("The file supplied is not a zip file")
+
     try:
-        archive = ZipFile(user_proposal, "r")
+        archive = ZipFile(proposal_zip, "r")
         file = archive.open("Proposal.xml")
     except KeyError:
-        raise KeyError("No file called Proposal.xml was found") from None
+        raise KeyError("The zip file contains no file Proposal.xml.") from None
 
-    tree = ElementTree.parse(file)
+    tree = parse(file)
     proposal = tree.getroot()
     if "code" not in proposal.attrib:
         raise ValueError("No proposal code supplied in the file Proposal.xml.")
-
-    if not zipfile.is_zipfile(user_proposal):
-        raise ValueError("The file supplied is not a zip file")
 
     if proposal.tag != "Proposal":
         raise ValueError("The root element in the file Proposal.xml is not called Proposal")
@@ -33,3 +33,6 @@ def get_proposal_code(user_proposal: Union[str, BinaryIO]) -> Optional[str]:
         return None
     else:
         raise ValueError(f"Invalid proposal code: {proposal_code}.")
+
+
+print(get_proposal_code("/home/lonwabolap/Downloads/Proposal.zip"))
