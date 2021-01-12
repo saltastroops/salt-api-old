0325fc3de9938c00ddaaf131b84f3ba043dd5003
Christian Hettlage <hettlage@saao.ac.za>
2021-01-12 14:45:55 +0200

    Updated tests

--- tests/test_proposal_code.py
+++ tests/test_proposal_code.py
@@ -1,92 +1,106 @@
-from zipfile import ZipFile
+"""Tests for extracting the proposal code."""
+
 from io import BytesIO
-from saltapi.repository.proposal_repository import get_proposal_code
+from zipfile import ZipFile
+
 import pytest
+
+from saltapi.repository.proposal_repository import get_proposal_code
 
 
 def create_zip(xml: bytes, filename: str = "Proposal.xml"):
+    """Create a zip file for the given XML content."""
     archive = BytesIO()
-    with ZipFile(archive, 'w') as zip_file:
+    with ZipFile(archive, "w") as zip_file:
         with zip_file.open(filename, "w") as proposal_file:
             proposal_file.write(xml)
     return archive
 
 
-# testing to see if the the file 'Proposal.xml' has the proposal code attribute
-def test_no_proposal_code():
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-          <Proposal  final="true">
-          </Proposal>'''
+def test_not_a_zip_file():
+    """Raise an error if the supplied file is not a zip file."""
     with pytest.raises(ValueError) as excinfo:
-        get_proposal_code(create_zip(file))
-    assert "No proposal code" in str(excinfo.value)
+        file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+               <Proposal xmlns:ns2="http://www.salt.ac.za/PIPT/Proposal/Shared/2.7"
+                         code="2020-2-SCI-043">
+               </Proposal>"""
+        get_proposal_code(BytesIO(file))
+    assert "not a zip file" in str(excinfo.value)
 
 
-# testing to see if the zipped folder provided has a file named 'Proposal.xml'
 def test_no_proposal_xml_file():
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-          <Proposal  code="2020-2-SCI-043" final="true">
-          </Proposal>'''
+    """Raise an error if the zip file has no file named 'Proposal.xml'."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+          <Proposal code="2020-2-SCI-043" final="true">
+          </Proposal>"""
     with pytest.raises(KeyError) as excinfo:
         get_proposal_code(create_zip(file, "file.xml"))
     assert "no file Proposal.xml" in str(excinfo.value)
 
 
-# testing to see if proposal code supplied in 'Proposal.xml' is valid
-def test_valid_proposal_code():
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-          <Proposal  code="2020-2-SCI-043" final="true">
-          </Proposal>'''
-    assert get_proposal_code(create_zip(file)) == "2020-2-SCI-043"
-
-
-# testing to see that an empty proposal code and other invalid variations will not be accepted
-@pytest.mark.parametrize("code", ["", "Unsubmitted-", "Unsubmitted-001"])
-def test_proposal_code_empty_and_unsubmitted(code):
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-          <Proposal code="%b" final="true">
-          </Proposal>''' % bytes(str(code), "utf-8")
-    assert get_proposal_code(create_zip(file)) is None
-
-
-# testing if the root element of the file 'Proposal.xml' is called 'Proposal'
 def test_root_element_is_not_called_proposal():
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-          <Proposals  code="2020-2-SCI-043" final="true">
-          </Proposals>'''
+    """Raise an error if the root element is not called 'Proposal'."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+          <Proposals code="2020-2-SCI-043" final="true">
+          </Proposals>"""
     with pytest.raises(ValueError) as excinfo:
         get_proposal_code(create_zip(file))
     assert "The root element in the file Proposal.xml" in str(excinfo.value)
 
 
-# testing that we get "Invalid proposal code" when proposal code does not start with 2 and other variations
-@pytest.mark.parametrize("code", [("1000-SCI-1-20", "submitted", "invalid")])
+def test_no_proposal_code():
+    """Raise an error if the root element doesn't have a code attribute."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+          <Proposal final="true">
+          </Proposal>"""
+    with pytest.raises(ValueError) as excinfo:
+        get_proposal_code(create_zip(file))
+    assert "No proposal code" in str(excinfo.value)
+
+
+@pytest.mark.parametrize("code", ["1000-SCI-1-20", "submitted", "invalid"])
 def test_invalid_proposal_code(code):
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+    """Raise an error if the proposal code is invalid."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
                <Proposal code="%b" final="true">
-               </Proposal>''' % bytes(str(code), "utf-8")
+               </Proposal>""" % bytes(
+        code, "utf-8"
+    )
     with pytest.raises(ValueError) as excinfo:
         get_proposal_code(create_zip(file))
     assert "Invalid proposal code" in str(excinfo.value)
 
 
-# When xml has namespaces, the root element is not cleanly given back, it's got the namespaces which we don't need
-# so we test that when xml has a namespace in the root element, we raise an exception'
-@pytest.mark.parametrize("code", [("2020-2-SCI-043", "2016-1-SCI-019")])
-def test_file_with_xml_namespace(code):
-    file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+@pytest.mark.parametrize(
+    "code", ["2", "2a", "2021", "2020-1-SCI-005", "2021-2-DDT-001"]
+)
+def test_valid_proposal_code(code):
+    """Valid proposal codes are parsed correctly."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+          <Proposal code="%b" final="true">
+          </Proposal>""" % bytes(
+        code, "utf-8"
+    )
+    assert get_proposal_code(create_zip(file)) == code
+
+
+@pytest.mark.parametrize("code", ["", "Unsubmitted-", "Unsubmitted-001"])
+def test_proposal_code_empty_or_unsubmitted(code):
+    """'Unsubmitted-...' proposal codes and empty strings are parsed as  None."""
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
+          <Proposal code="%b" final="true">
+          </Proposal>""" % bytes(
+        code, "utf-8"
+    )
+    assert get_proposal_code(create_zip(file)) is None
+
+
+def test_file_with_xml_namespace():
+    """Namespaces are supported."""
+    code = "2020-2-SCI-009"
+    file = b"""<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
                <ns2:Proposal xmlns:ns2="http://www.salt.ac.za/PIPT/Proposal/Shared/2.7"
-                code="%b"></ns2:Proposal>''' % bytes(str(code), "utf-8")
-    with pytest.raises(ValueError) as excinfo:
-        get_proposal_code(create_zip(file))
-    assert "The root element in the file Proposal.xml" in str(excinfo.value)
-
-
-# test that the folder supplied is a zipped folder otherwise raise exception
-def test_proposal_xml_folder_is_zipped():
-    with pytest.raises(ValueError) as excinfo:
-        file = b'''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-               <ns2:Proposal xmlns:ns2="http://www.salt.ac.za/PIPT/Proposal/Shared/2.7" code="2020-2-SCI-043">
-               </ns2:Proposal>'''
-        get_proposal_code(BytesIO(file))
-    assert "not a zip file" in str(excinfo.value)
+                code="%b"></ns2:Proposal>""" % bytes(
+        code, "utf-8"
+    )
+    assert get_proposal_code(create_zip(file)) == code
